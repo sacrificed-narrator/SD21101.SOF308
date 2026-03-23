32722f2efc4307b990c645867237400ce14d878b
Nguyen Vo Vinh Loc <vinhlloc2006@gmail.com>
2026-03-23 22:51:35 +0700

    Lab 4

---
+++ Lab4/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Hello from './components/Hello.vue'
+import Bai3 from './components/Bai3.vue'
+import Bai4 from './components/Bai4.vue'
+</script>
+
+<template>
+  <Hello />
+  <Bai3 />
+  <Bai4 />
+</template>
+
+<style scoped></style>
---
+++ Lab4/src/components/bai3.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Hiển thị lời chào -->
+  <h1>{{ greeting }}</h1>
+
+  <!-- Tạo nút thay đổi nội dung -->
+  <button v-bind:title="buttonTitle" @click="changeGreeting">Click để thay đổi</button>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            greeting: 'Xin chào bạn!',
+            buttonTitle: 'Nhấn để thay đổi lời chào'
+        };
+    },
+    methods: {
+        changeGreeting() {
+            this.greeting = 'Hello, World!';
+            this.buttonTitle = 'Lời chào mới đã được cập nhật';
+        }
+    }
+}
+</script>
